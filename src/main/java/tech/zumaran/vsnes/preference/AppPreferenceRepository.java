e1dfda81baa9114ada219afbd33691c35f7895f5
jc <jc@pop-os.localdomain>
2020-09-28 16:31:30 +0200

    Update project

--- src/main/java/tech/zumaran/vsnes/preference/AppPreferenceRepository.java
+++ src/main/java/tech/zumaran/vsnes/preference/AppPreferenceRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import tech.zumaran.genesis.GenesisRepository;
 
+@Repository
 public interface AppPreferenceRepository extends GenesisRepository<AppPreference> {
 
 	@Query("FROM AppPreference p WHERE p.key = :key AND p.deleted = 0")
